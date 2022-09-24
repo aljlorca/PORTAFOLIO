371dcee13524e4b1aa24b5eb8ac813c8e0228227
Alan Lorca <alan.espinoza.1133@gmail.com>
2022-09-23 23:12:33 -0300

    modificacion de configuracion

--- APIS/API_PRODUCTO/API_PRODUCTO/settings.py
+++ APIS/API_PRODUCTO/API_PRODUCTO/settings.py
@@ -78,9 +78,12 @@ WSGI_APPLICATION = 'API_PRODUCTO.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'localhost:1521/xe',
+        'USER': 'C##MERCADOCHILE',
+        'PASSWORD': '1133', 
+    },  
+    
 }
 
 
