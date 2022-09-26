29fef511e71f4788a8767699190bc239a3089e12
Alan Lorca <alan.espinoza.1133@gmail.com>
2022-09-26 19:34:40 -0300

    c

--- APIS/API_LOGIN/API/urls.py
+++ APIS/API_LOGIN/API/urls.py
@@ -6,6 +6,6 @@ from .views import ClienteExternoAuthView,ClienteInternoAuthView,ProveedorAuthVi
 urlpatterns = [
     path('cliente_externo_auth/',ClienteExternoAuthView.as_view(),name='cliente_externo_update'),
     path('cliente_interno_auth/',ClienteInternoAuthView.as_view(),name='cliente_interno_update'),
-    path('cliente_proveedor_auth/',ProveedorAuthView.as_view(),name='cliente_proveedor_update'),
-    path('cliente_transportista_auth/',TransportistaAuthView.as_view(),name='cliente_transportista_update'),
+    path('proveedor_auth/',ProveedorAuthView.as_view(),name='proveedor_update'),
+    path('transportista_auth/',TransportistaAuthView.as_view(),name='transportista_update'),
 ]
