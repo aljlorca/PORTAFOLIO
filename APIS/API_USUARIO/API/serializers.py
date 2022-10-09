3292d6b2287eebf44fcb762d1d3add5fd8a1710e
Alan Lorca <alan.espinoza.1133@gmail.com>
2022-10-09 18:48:41 -0300

    a

--- APIS/API_USUARIO/API/serializers.py
+++ APIS/API_USUARIO/API/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from .models import Usuario
+from .models import Usuario,Cargo
+
+
+class CargoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cargo
+        fields = '__all__'
 
 class UsuarioSerializer(serializers.ModelSerializer):
+    cargo = CargoSerializer(read_only=True)
     class Meta:
         model = Usuario
         fields = ['id_usuario','numero_identificacion_usuario','nombre_usuario','direccion_usuario','telefono_usuario','correo_usuario','id_cargo','id_empresa','id_ciudad']
