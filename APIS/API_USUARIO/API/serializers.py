ec7ce6ee16df1b2d633fd69857722cbe2c671485
Alan Lorca <alan.espinoza.1133@gmail.com>
2022-10-09 19:13:55 -0300

    cambio en serializer

--- APIS/API_USUARIO/API/serializers.py
+++ APIS/API_USUARIO/API/serializers.py
@@ -8,15 +8,18 @@ class CargoSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class UsuarioSerializer(serializers.ModelSerializer):
+    
+    nombre_cargo = serializers.CharField(read_only=True,source='cargo.nombre')
     cargo = CargoSerializer(read_only=True)
     class Meta:
         model = Usuario
-        fields = ['id_usuario','numero_identificacion_usuario','nombre_usuario','direccion_usuario','telefono_usuario','correo_usuario','id_cargo','id_empresa','id_ciudad']
+        fields = ['id_usuario','numero_identificacion_usuario','nombre_usuario','direccion_usuario','telefono_usuario','correo_usuario','id_cargo','id_empresa','id_ciudad','nombre_cargo','cargo']
 
 
 
 class UsuarioHistoricoSerializer(serializers.ModelSerializer):
-
+    nombre_cargo = serializers.CharField(read_only=True,source='cargo.nombre')
+    cargo = CargoSerializer(read_only=True)
     class Meta:
         model = Usuario
         fields = '__all__'
