955be2bdf26f1f197a616332c9ef76cac0bb6ab7
SebaCarrascoM <seba.carrasco121@hotmail.com>
2022-10-18 02:48:08 -0300

    carga

---
+++ App_Escritorio/MercadoChile/Negocio/Carga.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class Carga
+    {
+        public string capacidad_carga { get; set; }
+        public string refrigeracion_carga { get; set; }
+        public string tamano_carga { get; set; }
+        public int id_subasta { get; set; }
+        public int id_usuario { get; set; }
+    }
+}
