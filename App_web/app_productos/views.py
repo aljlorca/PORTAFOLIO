3d1fb255a08d0d85001c4216ecd2cc69e06fd5f5
Alan Lorca <alan.espinoza.1133@gmail.com>
2022-10-18 22:05:03 -0300

    bug fix

--- App_web/app_productos/views.py
+++ App_web/app_productos/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render,redirect
 from .controllers import pedido_get, productos_get
+from app.controllers import get_session
 
 
 
 # Create your views here.
 def pedido(request):
+    
     try:
         cargo=request.session['cargo']
         if cargo!='Proveedor' and 'Cliente Interno' and 'Cliente Externo':
@@ -12,9 +14,21 @@ def pedido(request):
 
     except:
         pass
+
+    usuario=request.session['username']
+    cargo=request.session['cargo']
+    correo=request.session['email']
+    empresa=request.session['company']
+    id_usuario = request.session['id_user']
     
-    resp = {
+    data = {
         'pedidos':pedido_get(),
         'producto':productos_get(),
+        'cargo':cargo,
+        'usuario':usuario,
+        'correo':correo,
+        'empresa':empresa,
+        'id_user':id_usuario
     }
-    return render(request, 'app/productos.html',resp)
+
+    return render(request, 'app/productos.html',data)
