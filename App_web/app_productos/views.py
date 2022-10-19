457feca4e232ab5716127b3e48af5b238c77439c
Alan Lorca <alan.espinoza.1133@gmail.com>
2022-10-18 21:42:21 -0300

    cambios

--- App_web/app_productos/views.py
+++ App_web/app_productos/views.py
@@ -5,12 +5,16 @@ from .controllers import pedido_get, productos_get
 
 # Create your views here.
 def pedido(request):
-    cargo=request.session['cargo']
-    if cargo!='Proveedor' and 'Cliente Interno' and 'Cliente Externo':
-        return redirect(to="http://127.0.0.1:3000/")
+    try:
+        cargo=request.session['cargo']
+        if cargo!='Proveedor' and 'Cliente Interno' and 'Cliente Externo':
+            return redirect(to="http://127.0.0.1:3000/")
+
+    except:
+        pass
+    
     resp = {
         'pedidos':pedido_get(),
         'producto':productos_get(),
-        'cargo':cargo,
     }
     return render(request, 'app/productos.html',resp)
