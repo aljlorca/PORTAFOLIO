972e64d1b2687ed7f24c98928dd5f7c8a633c051
Alan Espinoza <alan.espinoza@gmail.com>
2022-10-20 21:37:29 -0300

    ordenado de controllers

--- App_web/app/controllers.py
+++ App_web/app/controllers.py
@@ -3,6 +3,8 @@ import json
 from django.contrib.sessions import *
 from django.http import HttpResponse
 
+
+#AUTH CONTROLLERS 
 def login_controller(correo,contrasena):
     url = 'http://127.0.0.1:8006/api/usuario_auth/'
     body = {"correo_usuario": correo,"contrasena_usuario": contrasena}
@@ -30,21 +32,6 @@ def logout_controller(request):
         pass
     return HttpResponse("Has cerrado Session")
 
-def crear_producto(id_producto,nombre_producto,cantidad_producto,precio_producto,imagen_producto,id_calidad,saldo_producto,estado_fila,id_usuario): 
-
-    url= 'http://127.0.0.1:8010/api/producto/'
-    files = {"imagen_producto":open(imagen_producto, 'rb')}
-    body ={"id_producto":id_producto,"nombre_producto":nombre_producto,"cantidad_producto":cantidad_producto,"precio_producto":precio_producto,"id_calidad":id_calidad,"saldo_producto":saldo_producto,"estado_fila":estado_fila,"id_usuario":id_usuario}
-    response = requests.post(url,data=body, files=files)
-    return response
-
-def subasta_controller(monto,id_venta,id_usuario):
-    url = 'http://127.0.0.1:8014/api/subasta_old/'
-    body = {"monto_subasta": monto,"id_venta":id_venta,"id_usuario":id_usuario}
-    response = requests.post(url,json=body)
-    print (response)
-
-
 def get_session(request):
     try:
         usuario=request.session['username']
@@ -66,7 +53,31 @@ def get_session(request):
 
 
 
+#PRODUCTOS CONTROLLERS
+def crear_producto(id_producto,nombre_producto,cantidad_producto,precio_producto,imagen_producto,id_calidad,saldo_producto,estado_fila,id_usuario): 
 
+    url= 'http://127.0.0.1:8010/api/producto/'
+    files = {"imagen_producto":open(imagen_producto, 'rb')}
+    body ={"id_producto":id_producto,"nombre_producto":nombre_producto,"cantidad_producto":cantidad_producto,"precio_producto":precio_producto,"id_calidad":id_calidad,"saldo_producto":saldo_producto,"estado_fila":estado_fila,"id_usuario":id_usuario}
+    response = requests.post(url,data=body, files=files)
+    return response
+
+def productos_get():
+    url='http://127.0.0.1:8010/api/producto/'
+    r = requests.get(url)
+    if r.status_code == 200:
+        content = json.loads(r.content)
+        return content
+
+
+#SUBASTA CONTROLLERS
+def subasta_controller(monto,id_venta,id_usuario):
+    url = 'http://127.0.0.1:8014/api/subasta_old/'
+    body = {"monto_subasta": monto,"id_venta":id_venta,"id_usuario":id_usuario}
+    response = requests.post(url,json=body)
+    print (response)
+
+#PEDIDO CONTROLLERS
 def pedido_get():
     url = 'http://127.0.0.1:8008/api/pedido/'
     r = requests.get(url)
@@ -74,9 +85,3 @@ def pedido_get():
         content = json.loads(r.content)
         return content
     
-def productos_get():
-    url='http://127.0.0.1:8010/api/producto/'
-    r = requests.get(url)
-    if r.status_code == 200:
-        content = json.loads(r.content)
-        return content
