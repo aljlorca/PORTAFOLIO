6a4d0ea73b138cbe01323870b630517105bad94c
Alan Espinoza <alan.espinoza@gmail.com>
2022-10-27 00:03:23 -0300

    carga exceptions

--- APIS/API_CARGA/API/views.py
+++ APIS/API_CARGA/API/views.py
@@ -16,19 +16,21 @@ def agregar_carga(capacidad_carga,refrigeracion,tamano_carga,id_subasta,id_usuar
     cursor = django_cursor.connection.cursor()
     salida = cursor.var(cx_Oracle.NUMBER)
     cursor.callproc('CARGA_AGREGAR',[capacidad_carga,refrigeracion,tamano_carga,id_subasta,id_usuario,salida])
-    return salida
+    return round(salida.getvalue())
 
 def modificar_carga(id_carga,capacidad_carga,refrigeracion,tamano_carga,id_subasta,id_usuario):
     django_cursor = connection.cursor()
     cursor = django_cursor.connection.cursor()
     salida = cursor.var(cx_Oracle.NUMBER)
     cursor.callproc('CARGA_MODIFICAR',[id_carga,capacidad_carga,refrigeracion,tamano_carga,id_subasta,id_usuario,salida])
+    return round(salida.getvalue())
 
 def eliminar_carga(id_carga):
     django_cursor = connection.cursor()
     cursor = django_cursor.connection.cursor()
     salida = cursor.var(cx_Oracle.NUMBER)
     cursor.callproc('CARGA_ELIMINAR',[id_carga,salida])
+    return round(salida.getvalue())
 
 def lista_carga():
     django_cursor = connection.cursor()
@@ -65,8 +67,14 @@ class CargaView(View):
 
     def post(self, request):
         jd = json.loads(request.body)
-        agregar_carga(capacidad_carga=jd['capacidad_carga'],refrigeracion=jd['refrigeracion'],tamano_carga=jd['tamano_carga'],id_subasta=jd['id_subasta'],id_usuario=jd['id_usuario'])
-        datos = {'message':'Success'}
+        try: 
+            salida = agregar_carga(capacidad_carga=jd['capacidad_carga'],refrigeracion=jd['refrigeracion'],tamano_carga=jd['tamano_carga'],id_subasta=jd['id_subasta'],id_usuario=jd['id_usuario'])
+            if salida == 1:
+                datos = {'message':'Success'}
+            elif salida == 0:
+                datos = {'message':'ERROR: No fue posible registrar la carga'}
+        except:
+            datos = {'message':'ERROR: Validar datos'}
         return JsonResponse(datos)
         
 
@@ -74,8 +82,14 @@ class CargaView(View):
         jd = json.loads(request.body)
         cargas = list(Carga.objects.filter(id_carga=id_carga).values())
         if len(cargas) > 0:
-            modificar_carga(id_carga=jd['id_carga'],capacidad_carga=jd['capacidad_carga'],refrigeracion=jd['refrigeracion'],tamano_carga=jd['tamano_carga'],id_subasta=jd['id_subasta'],id_usuario=jd['id_usuario'])
-            datos={'message':"Success"}
+            try:
+                salida = modificar_carga(id_carga=jd['id_carga'],capacidad_carga=jd['capacidad_carga'],refrigeracion=jd['refrigeracion'],tamano_carga=jd['tamano_carga'],id_subasta=jd['id_subasta'],id_usuario=jd['id_usuario'])
+                if salida == 1:
+                    datos={'message':"Success"}
+                elif salida == 0:
+                    datos = {'message':'ERROR: No fue posible actualizar la carga'}
+            except:
+                datos = {'message':'ERROR: Validar datos'}
         else:
             datos={'message':"ERROR: No se encuentra la carga"}
         return JsonResponse(datos)
@@ -83,8 +97,14 @@ class CargaView(View):
     def delete(self, request,id_carga):
         cargas = list(Carga.objects.filter(id_carga=id_carga).values())
         if len(cargas) > 0:
-            salida = eliminar_carga(id_carga)
-            datos={'message':"Success"}
+            try: 
+                salida = eliminar_carga(id_carga)
+                if salida == 1:
+                    datos={'message':"Success"}
+                elif salida == 0: 
+                    datos={'message':"ERROR: no fue posible eliminar la carga"}
+            except:
+                datos = {'message':'ERROR: Validar datos'}
         else:
             datos={'message':"ERROR: no fue posible eliminar el cargo"}
         return JsonResponse(datos)
