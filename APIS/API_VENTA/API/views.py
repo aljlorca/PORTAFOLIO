54ad6eaffdebf0ed045232598190a2c1ba432774
Alan Espinoza <alan.espinoza@gmail.com>
2022-11-02 23:15:05 -0300

    cambios en api venta

--- APIS/API_VENTA/API/views.py
+++ APIS/API_VENTA/API/views.py
@@ -3,6 +3,8 @@ from django.http.response import JsonResponse
 from django.views import View
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
+from rest_framework.response import Response
 from .models import Venta
 from .serializers import VentaSerializer,VentaHistoricoSerializer
 from rest_framework import viewsets
@@ -11,11 +13,10 @@ import cx_Oracle
 import datetime
 
 # Create your views here.
-def agregar_venta(id_venta,descripcion_venta,estado_venta,monto_bruto_venta,iva,monto_neto_venta,tipo_venta,id_usuario,cantidad_venta,monto_transporte,monto_aduanas,pago_servicio,comision_venta):
+def agregar_venta(id_venta,descripcion_venta,estado_venta,monto_bruto_venta,iva,monto_neto_venta,tipo_venta,id_usuario,fecha_venta,cantidad_venta,monto_transporte,monto_aduanas,pago_servicio,comision_venta):
     django_cursor = connection.cursor()
     cursor = django_cursor.connection.cursor()
     salida = cursor.var(cx_Oracle.NUMBER)
-    fecha_venta = datetime.date.today()
     estado_fila = '1'
     cursor.callproc('VENTA_AGREGAR',[id_venta,descripcion_venta,estado_venta,monto_bruto_venta,iva,monto_neto_venta,fecha_venta,tipo_venta,id_usuario,cantidad_venta,monto_transporte,monto_aduanas,pago_servicio,comision_venta,estado_fila,salida])
     return round(salida.getvalue())
@@ -71,15 +72,25 @@ class VentaView(View):
         try:
             jd = json.loads(request.body)
             try:
-                salida = agregar_venta(id_venta=jd['id_venta'],descripcion_venta=jd['descripcion_venta'],estado_venta=jd['estado_venta'],monto_bruto_venta=jd['monto_bruto_venta'],iva=jd['iva'],monto_neto_venta=jd['monto_neto_venta'],tipo_venta=jd['tipo_venta'],id_usuario=jd['id_usuario'],cantidad_venta=jd['cantidad_venta'],monto_transporte=jd['monto_transporte'],monto_aduanas=jd['monto_aduanas'],pago_servicio=jd['pago_servicio'],comision_venta=jd['comision_venta'])
+                salida = agregar_venta(id_venta=jd['id_venta'],
+                descripcion_venta=jd['descripcion_venta'],
+                estado_venta=jd['estado_venta'],
+                monto_bruto_venta=jd['monto_bruto_venta'],
+                iva=jd['iva'],monto_neto_venta=jd['monto_neto_venta'],
+                tipo_venta=jd['tipo_venta'],id_usuario=jd['id_usuario'],fecha_venta=jd['fecha_venta'],
+                cantidad_venta=jd['cantidad_venta'],monto_transporte=jd['monto_transporte'],monto_aduanas=jd['monto_aduanas'],pago_servicio=jd['pago_servicio'],comision_venta=jd['comision_venta'])
                 if salida == 1:
                     datos = {'message':'Success'}
+                    print(datos)
                 elif salida == 0:
                     datos = {'message':'ERORR: no fue posible agregar la venta'}
+                    print(datos)
             except:
                 datos = {'message':'ERROR: Validar datos'}
+                print(datos)
         except:
             datos = {'message':'ERORR: Json invalido'}
+            print(datos)
 
         return JsonResponse(datos)
         
