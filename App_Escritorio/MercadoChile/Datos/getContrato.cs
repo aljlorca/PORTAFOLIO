d78aec8671ac5ecf3a42eb0468215aa78bd1488f
Bdiaz1232 <bastian.diaz.caceres@gmail.com>
2022-11-16 01:24:17 -0300

    Modificacion de Creacion de Contratos

--- App_Escritorio/MercadoChile/Datos/getContrato.cs
+++ App_Escritorio/MercadoChile/Datos/getContrato.cs
@@ -11,10 +11,29 @@ namespace Datos
     public class getContrato
     {
         private string url = "http://127.0.0.1:8000/api_empresa/empresa/";
+        private string url1 = "http://127.0.0.1:8000/api_usuario/usuario/";
+        private string url2 = "http://127.0.0.1:8000/api_cargo/cargo/";
         public async Task<string> GetHttp()
         {
 
             WebRequest oRequest = WebRequest.Create(url);
+            WebResponse oResponse = oRequest.GetResponse();
+            StreamReader sr = new StreamReader(oResponse.GetResponseStream());
+            return await sr.ReadToEndAsync();
+        }
+
+        public async Task<string> GetHttp1()
+        {
+
+            WebRequest oRequest = WebRequest.Create(url1);
+            WebResponse oResponse = oRequest.GetResponse();
+            StreamReader sr = new StreamReader(oResponse.GetResponseStream());
+            return await sr.ReadToEndAsync();
+        }
+        public async Task<string> GetHttp2()
+        {
+
+            WebRequest oRequest = WebRequest.Create(url2);
             WebResponse oResponse = oRequest.GetResponse();
             StreamReader sr = new StreamReader(oResponse.GetResponseStream());
             return await sr.ReadToEndAsync();
