bfcd66d90b60942e7cf91848d88c191b48266d61
Alan Lorca <alan.espinoza.1133@gmail.com>
2022-12-07 18:40:26 -0300

    eliminado de url

--- API/login/urls.py
+++ API/login/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path,include
-from .views import UsuarioAuthView,UsuarioHistoricoViewset
+from .views import UsuarioAuthView
 from rest_framework import routers
 
-router = routers.DefaultRouter()
-router.register('usuario_desktop',UsuarioHistoricoViewset)
+
 
 urlpatterns = [
-    path('',include(router.urls)),
     path('usuario_auth/',UsuarioAuthView.as_view(),name='usuario_auth'),
 ]
