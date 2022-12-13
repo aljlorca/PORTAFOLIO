dad30f51dc18f3d5225b7192034c4e4b02c402a8
Alan Lorca <alan.espinoza.1133@gmail.com>
2022-12-13 19:13:13 -0300

    agregado de url faltante

--- API/subasta/urls.py
+++ API/subasta/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('subasta_old/<int:id_subasta>',SubastaView.as_view(),name='subasta_update'),
     path('subasta_venta/<int:id_venta>',SubastaVentaView.as_view(),name='subasta_venta'),
     path('subasta_aceptar/',SubastaAceptarView.as_view(),name='subasta_aceptar'),
+    path('subasta_aceptar/<str:id_subasta>',SubastaAceptarView.as_view(),name='subasta_aceptar'),
     path('subasta_venta_usuario/<str:id_usuario>',SubastaVentaUserView.as_view(),name='subasta_venta_usuario'),
     path('subasta_aceptada/<str:id_venta>',SubastaAceptada.as_view(),name='subasta_aceptada'),
     path('subasta_carga/<str:id_venta>',SubastaCargaView.as_view(),name='subasta_carga'),
